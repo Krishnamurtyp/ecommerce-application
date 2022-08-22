e7c2ed4b0d1a5fbe7d2b6a9b0a9f56361ab76456
krishnamurtyp <krishnamurthy.pradhan@gmail.com>
2022-08-22 08:46:11 +0530

    First commit

---
+++ server/common-data-service/src/main/java/com/iiht/ecommerce/commondataservice/CommonDataServiceApplication.java
@@ -0,0 +1,18 @@
+package com.iiht.ecommerce.commondataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.iiht.ecommerce.commondataservice.controller.CommonDataController;
+
+@SpringBootApplication
+@EnableCaching
+public class CommonDataServiceApplication {
+
+    public static void main(final String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(CommonDataServiceApplication.class, args);
+        context.getBean(CommonDataController.class).fillWithTestData();
+    }
+}
